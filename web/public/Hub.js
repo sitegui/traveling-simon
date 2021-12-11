67fedb38eb2a8c2c968ab366e3907372cc9d0b8d
Guilherme <sitegui@sitegui.com.br>
2021-12-11 22:27:12 +0100

    Show calculated paths

--- web/public/Hub.js
+++ web/public/Hub.js
@@ -8,6 +8,7 @@ class Hub {
     this.rideDurations = new RideDurations()
     this.minStartAt = '09:00'
     this.maxEndAt = null
+    this.pathPolyline = null
 
     // eslint-disable-next-line no-template-curly-in-string
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -19,7 +20,14 @@ class Hub {
 
     this.showSitesPane = $('#show-sites-pane')
     this.editSitePane = $('#edit-site-pane')
-    this.calculatingPathsPanel = $('#calculating-paths-pane')
+    this.calculatingPathsPane = $('#calculating-paths-pane')
+    this.showPathsPane = $('#show-paths-pane')
+    this.panes = {
+      showSites: this.showSitesPane,
+      editSite: this.editSitePane,
+      calculatingPaths: this.calculatingPathsPane,
+      showPaths: this.showPathsPane
+    }
 
     // Start edit-site pane
     $('#site-duty-add').onclick = event => {
@@ -53,6 +61,11 @@ class Hub {
       this.persistStorage()
     }
 
+    // Start show-paths pane
+    $('.back', this.showPathsPane).onclick = () => {
+      this.showSites()
+    }
+
     // Reload persisted data
     const data = Hub.loadStorage()
     if (data) {
@@ -78,6 +91,18 @@ class Hub {
     this.showSites()
   }
 
+  switchPane (name) {
+    for (const [paneName, pane] of Object.entries(this.panes)) {
+      pane.classList.toggle('d-none', paneName !== name)
+    }
+    this.editingSite = null
+    if (this.pathPolyline) {
+      this.pathPolyline.remove()
+      this.pathPolyline = null
+    }
+    this.persistStorage()
+  }
+
   addSite (latitude, longitude) {
     const name = `Site ${this.sites.length + 1}`
     const site = new Site(name, latitude, longitude)
@@ -88,10 +113,8 @@ class Hub {
   }
 
   editSite (site) {
+    this.switchPane('editSite')
     this.editingSite = site
-    show(this.editSitePane)
-    hide(this.showSitesPane)
-    hide(this.calculatingPathsPanel)
     for (const otherSite of this.sites) {
       otherSite.marker.setOpacity(otherSite === site ? 1.0 : 0.5)
     }
@@ -154,10 +177,7 @@ class Hub {
   }
 
   showSites () {
-    this.editingSite = null
-    hide(this.editSitePane)
-    show(this.showSitesPane)
-    hide(this.calculatingPathsPanel)
+    this.switchPane('showSites')
 
     for (const el of $$('.site', this.showSitesPane)) {
       el.remove()
@@ -210,15 +230,93 @@ class Hub {
   }
 
   calculatePaths () {
-    hide(this.editSitePane)
-    hide(this.showSitesPane)
-    show(this.calculatingPathsPanel)
+    this.switchPane('calculatingPaths')
 
     this.rideDurations.updateForSites(this.sites).then(() =>
       postApi('/api/calculate-paths', this.prepareCalculationWorld())
     ).then(paths => {
-      console.log(paths)
+      this.showPaths(paths)
     })
+  }
+
+  showPaths (paths) {
+    this.switchPane('showPaths')
+    for (const row of $$('.path', this.showPathsPane)) {
+      row.remove()
+    }
+
+    let activeRow = null
+    const template = $('#path-template')
+    for (const path of paths) {
+      const row = this.cloneTemplate(template)
+      row.classList.add('path')
+      $('.path-total-ride', row).textContent = path.cost.totalRide
+      $('.path-total-time', row).textContent = path.cost.totalTime
+      $('.path-stops-on-duty', row).textContent = path.cost.stopsOnDuty
+      $('.path-stops', row).textContent = path.cost.stops
+      row.onclick = () => {
+        if (activeRow) {
+          activeRow.classList.remove('table-active')
+        }
+        row.classList.add('table-active')
+        activeRow = row
+        this.showPath(path)
+      }
+      $('.paths', this.showPathsPane).appendChild(row)
+    }
+
+    hide($('.detailed-path', this.showPathsPane))
+  }
+
+  showPath (path) {
+    const visitedSiteNames = new Set(path.stops.map(stop => stop.site))
+    const siteByName = new Map(this.sites.map(site => [site.name, site]))
+
+    for (const site of this.sites) {
+      site.marker.setOpacity(visitedSiteNames.has(site.name) ? 1.0 : 0.5)
+    }
+
+    const latlngs = [siteByName.get(path.startIn).marker.getLatLng()]
+    for (const stop of path.stops) {
+      latlngs.push(siteByName.get(stop.site).marker.getLatLng())
+    }
+
+    if (this.pathPolyline) {
+      this.pathPolyline.remove()
+    }
+    this.pathPolyline = L.polyline(latlngs, { color: 'black', weight: 5 }).addTo(this.map)
+    this.map.fitBounds(this.pathPolyline.getBounds())
+
+    show($('.detailed-path', this.showPathsPane))
+    const steps = [
+      [path.startAt, `Start from ${path.startIn}`]
+    ]
+    for (const stop of path.stops) {
+      if (stop.rideStart !== path.startAt) {
+        steps.push([stop.rideStart, `Ride to ${stop.site}`])
+      }
+      if (stop.duty) {
+        steps.push([stop.rideEnd, `Arrive at ${stop.site}. Duty is from ${stop.duty.start} until ${stop.duty.end}`])
+      } else {
+        steps.push([stop.rideEnd, `Arrive at ${stop.site}`])
+      }
+      if (stop.serviceStart !== stop.rideEnd) {
+        steps.push([stop.serviceStart, `Wait in ${stop.site} for duty start`])
+      }
+    }
+
+    const template = $('#step-template')
+    const pathStepsEl = $('.path-steps', this.showPathsPane)
+    for (const step of $$('.step', this.showPathsPane)) {
+      step.remove()
+    }
+    for (const [time, text] of steps) {
+      const stepEl = this.cloneTemplate(template)
+      stepEl.classList.add('step')
+      $('.step-time', stepEl).textContent = time
+      $('.step-text', stepEl).textContent = text
+      pathStepsEl.insertBefore(stepEl, pathStepsEl.firstChild)
+    }
   }
 
   prepareCalculationWorld () {
